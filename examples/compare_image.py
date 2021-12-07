c08d8cce368df26fb7ce19ff7db64206bdd872cf
Zhenyu Tang <zhy@cs.umd.edu>
2021-12-07 18:00:06 -0500

    update image method script

--- examples/compare_image.py
+++ examples/compare_image.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pygsound as ps
 import matplotlib.pyplot as plt
-import rirgenerator as rg
-
+import rir_generator as rir
+# using https://github.com/audiolabs/rir-generator/
 
 def main():
     l = 10
     w = 6
-    h = 2
+    h = 4
     absorb = 0.3
     reflec = np.sqrt(1.0 - absorb)
 
@@ -17,7 +17,7 @@ def main():
     ctx.channel_type = ps.ChannelLayoutType.mono
     scene = ps.Scene()
 
-    mesh = ps.createbox(l, w, h, absorb, 0.5)
+    mesh = ps.createbox(l, w, h, absorb, 0.1)
     scene.setMesh(mesh)
 
     src_coord = [1, 1, 0.5]
@@ -30,14 +30,21 @@ def main():
     lis.radius = 0.01
 
     rir_gs = scene.computeIR(src, lis, ctx)
-    rir_img = rg.rir_generator(343, 16000, lis_coord, src_coord, [l, w, h], beta=[reflec]*6)
-    rir_img = rir_img / max(abs(rir_img[0]))
+    rir_img = rir.generate(
+        c=343,  # Sound velocity (m/s)
+        fs=ctx.sample_rate,  # Sample frequency (samples/s)
+        r=[lis_coord],
+        s=src_coord,  # Source position [x y z] (m)
+        L=[l, w, h],  # Room dimensions [x y z] (m)
+        beta=[reflec] * 6,  # Reflection coefficients
+    )
+    rir_img = rir_img[:, 0] / max(abs(rir_img))
 
-    max_cnt = min(len(rir_gs['samples']), len(rir_img[0])) * 2
+    max_cnt = min(len(rir_gs['samples']), len(rir_img))
 
-    fig, axs = plt.subplots(4, 1, figsize=(10, 20))
+    fig, axs = plt.subplots(2, 1, figsize=(16, 10))
     axs[0].set_title('Image Method (waveform)')
-    axs[0].plot(rir_img[0], linewidth=0.5)
+    axs[0].plot(rir_img, linewidth=0.5)
     axs[0].set_xlabel('Sample')
     axs[0].set_xlim(0, max_cnt)
     axs[0].set_ylim(-1, 1)
@@ -49,18 +56,6 @@ def main():
     axs[1].set_xlim(0, max_cnt)
     axs[1].set_ylim(-1, 1)
     axs[1].set_ylabel('Amplitude')
-
-    axs[2].set_title('Image Method (spectrogram)')
-    axs[2].specgram(rir_img[0], mode='magnitude', NFFT=1024, Fs=16000, noverlap=512)
-    axs[2].set_xlim(0, max_cnt / 16000)
-    axs[2].set_xlabel('Times (s)')
-    axs[2].set_ylabel('Frequency (Hz)')
-
-    axs[3].set_title('Geometric Sound Propagation (spectrogram)')
-    axs[3].specgram(rir_gs['samples'], mode='magnitude', NFFT=1024, Fs=16000, noverlap=512)
-    axs[3].set_xlim(0, max_cnt / 16000)
-    axs[3].set_xlabel('Times (s)')
-    axs[3].set_ylabel('Frequency (Hz)')
 
     fig.tight_layout()
     plt.savefig('img_comparison.png')
